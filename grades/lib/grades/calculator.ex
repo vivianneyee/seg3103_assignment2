122bc7f60caa1963156a007497347dae6d29dc69
Vivienne Cruz <cruzviv27@hotmail.com>
2021-06-10 12:38:03 -0400

    refactored such that return mark code can be reused

--- grades/lib/grades/calculator.ex
+++ grades/lib/grades/calculator.ex
@@ -12,6 +12,38 @@ defmodule Grades.Calculator do
     0.2 * avg_labs + 0.3 * avg_homework + 0.2 * midterm + 0.3 * final
   end
 
+  def return_mark(mark, type) do
+    if type == "letter" do
+      cond do
+        mark > 0.895 -> "A+"
+        mark > 0.845 -> "A"
+        mark > 0.795 -> "A-"
+        mark > 0.745 -> "B+"
+        mark > 0.695 -> "B"
+        mark > 0.645 -> "C+"
+        mark > 0.595 -> "C"
+        mark > 0.545 -> "D+"
+        mark > 0.495 -> "D"
+        mark > 0.395 -> "E"
+        :else -> "F"
+      end
+    else
+      cond do
+        mark > 0.895 -> 10
+        mark > 0.845 -> 9
+        mark > 0.795 -> 8
+        mark > 0.745 -> 7
+        mark > 0.695 -> 6
+        mark > 0.645 -> 5
+        mark > 0.595 -> 4
+        mark > 0.545 -> 3
+        mark > 0.495 -> 2
+        mark > 0.395 -> 1
+        :else -> 0
+      end
+    end
+  end
+
   def failed_to_participate(avg_homework, avg_exams, avg_labs, num_labs, midterm, final, type) do
     if avg_homework < 0.4 || avg_exams < 0.4 || num_labs < 3 do
       if type == "letter" do
@@ -21,35 +53,7 @@ defmodule Grades.Calculator do
       end
     else
       mark = calculate_grade(avg_labs, avg_homework, midterm, final)
-      if type == "letter" do
-        cond do
-          mark > 0.895 -> "A+"
-          mark > 0.845 -> "A"
-          mark > 0.795 -> "A-"
-          mark > 0.745 -> "B+"
-          mark > 0.695 -> "B"
-          mark > 0.645 -> "C+"
-          mark > 0.595 -> "C"
-          mark > 0.545 -> "D+"
-          mark > 0.495 -> "D"
-          mark > 0.395 -> "E"
-          :else -> "F"
-        end
-      else
-        cond do
-          mark > 0.895 -> 10
-          mark > 0.845 -> 9
-          mark > 0.795 -> 8
-          mark > 0.745 -> 7
-          mark > 0.695 -> 6
-          mark > 0.645 -> 5
-          mark > 0.595 -> 4
-          mark > 0.545 -> 3
-          mark > 0.495 -> 2
-          mark > 0.395 -> 1
-          :else -> 0
-        end
-      end
+      return_mark(mark, type)
     end
   end
 
