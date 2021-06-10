97771e76802b8e24d62600893c1a949f10f9621a
Vivianne Yee <yee.vivianne@gmail.com>
2021-06-10 11:57:11 -0400

    created helper method for calculating grade

--- grades/lib/grades/calculator.ex
+++ grades/lib/grades/calculator.ex
@@ -8,12 +8,16 @@ defmodule Grades.Calculator do
     end
   end
 
+  def calculate_grade(avg_labs, avg_homework, midterm, final) do
+    0.2 * avg_labs + 0.3 * avg_homework + 0.2 * midterm + 0.3 * final
+  end
+
   def percentage_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
     avg_homework = avg(homework)
 
     avg_labs = avg(labs)
 
-    mark = 0.2 * avg_labs + 0.3 * avg_homework + 0.2 * midterm + 0.3 * final
+    mark = calculate_grade(avg_labs, avg_homework, midterm, final)
     round(mark * 100)
   end
 
@@ -32,7 +36,7 @@ defmodule Grades.Calculator do
     if avg_homework < 0.4 || avg_exams < 0.4 || num_labs < 3 do
       "EIN"
     else
-      mark = 0.2 * avg_labs + 0.3 * avg_homework + 0.2 * midterm + 0.3 * final
+      mark = calculate_grade(avg_labs, avg_homework, midterm, final)
 
       cond do
         mark > 0.895 -> "A+"
@@ -65,7 +69,7 @@ defmodule Grades.Calculator do
     if avg_homework < 0.4 || avg_exams < 0.4 || num_labs < 3 do
       0
     else
-      mark = 0.2 * avg_labs + 0.3 * avg_homework + 0.2 * midterm + 0.3 * final
+      mark = calculate_grade(avg_labs, avg_homework, midterm, final)
 
       cond do
         mark > 0.895 -> 10
